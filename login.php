b0671bd86cedf50ea7b1e01daf4fbedd1f2512b6
Abhilaash <Abhilaash@users.noreply.github.com>
2015-08-01 22:39:44 -0400

    Update login.php

--- login.php
+++ login.php
@@ -17,7 +17,37 @@
 	<input type="password" name="password" id="username"><br>
 	&nbsp;&nbsp;<button type="submit" id = "login" accesskey = "l" >Log In</button>&nbsp;&nbsp;
 	</form>
-	</body>
+	</body><!DOCTYPE html>
+<html>
+	<head>
+		 <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css">
+    <link rel="stylesheet" href="login.css">
+	</head>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+        	<img src="planit logo1.png" height = "400" width = "350">
+        	<div class = "spacing">
+
+        	</div>
+            <!-- <h1 class="text-center login-title"><font color="white">Login</font></h1> -->
+            <div class="account-wall">
+
+                <form class="form-signin">
+                <!-- <label><font color="white">Username:</font></label> -->
+                <input type="username" class="form-control" placeholder="Username" required>
+                <!-- <label><font color="white">Password:</font></label> -->
+                <input type="password" class="form-control" placeholder="Password" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">
+                    Sign in</button>
+    
+            </div>
+            <a href="#" class="text-center new-account">Create an account </a>
+        </div>
+    </div>
+</div>
 	<?php
 		class Database extends SQLite3 {
 			function __construct() {
