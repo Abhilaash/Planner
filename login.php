c4169da6e500924e17024bd3e120cc97bc4b70c9
Abhilaash <Abhilaash.Velamati@gmail.com>
2015-08-02 06:16:26 -0400

    update
    UPdated the name and id

--- login.php
+++ login.php
@@ -18,8 +18,8 @@
                 <form class="form-signin">
                 <center><h1> <font color="#19A3FF">Login </font></h1><Center>
                 <center><img src="planitlogosidewaysblack.png" height = "70%" width = "70%"></center>
-                <input type="text" class="form-control" placeholder="Username" required autofocus>
-                <input type="password" class="form-control" placeholder="Password" required>
+                <input type="text" class="form-control" placeholder="Username" name = "username" id = "username" required autofocus>
+                <input type="password" class="form-control" placeholder="Password" name = "password" id = "password" required>
                 <button class="btn btn-lg btn-primary btn-block" type="submit"><a href "userprofile.html">
                     Sign in </a></button>
                 <a href="#" class="text-center new-account">Create an account </a>
